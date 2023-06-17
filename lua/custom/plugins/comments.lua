cb623a4493c65cb07b6040768cd60d425d9860f7
Raymond <rjrosales2025@gmail.com>
2023-06-16 21:44:58 -0700

    added cool todo stuff

--- lua/custom/plugins/comments.lua
+++ lua/custom/plugins/comments.lua
@@ -6,4 +6,13 @@ return {
             require('mini.comment').setup()
         end
     },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        },
+    },
 }
