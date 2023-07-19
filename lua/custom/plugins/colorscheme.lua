3b12c7e703eb790e3eb87c7eb212948b3d6147f4
Raymond <rjrosales2025@gmail.com>
2023-07-18 23:00:06 -0400

    added cool color schemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,20 +4,25 @@ return {
         'maxmx03/fluoromachine.nvim',
         priority = 1000,
         config = function ()
-            local fm = require 'fluoromachine'
-
-            fm.setup {
+            require 'fluoromachine'.setup {
                 glow = false,
                 theme = 'fluoromachine'
             }
-
-            -- vim.cmd.colorscheme 'fluoromachine'
-            vim.cmd.colorscheme 'solas'
+            vim.cmd.colorscheme 'fluoromachine'
+            -- vim.cmd.colorscheme 'solas'
             -- Yeah, this is hella scuffed. I know.
         end
     },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        -- priority = 1000,
+        opts = {},
+    },
     -- bluish greenish
-    'damage220/solas.vim',
+    'damage220/solas.vim', -- THIS DOESN'T WORK WELL WITH NEORG
     'jackiehluo/vim-material',
     'rebelot/kanagawa.nvim',
+    'ajmwagar/vim-deus',
+    'morhetz/gruvbox',
 }
