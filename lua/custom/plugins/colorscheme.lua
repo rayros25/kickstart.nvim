d0a479f45e30ba9d4e3a3df170a40ee2ab34cda5
Raymond <rjrosales2025@gmail.com>
2023-07-20 21:51:52 -0400

    tweak colors some more

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -5,8 +5,16 @@ return {
         priority = 1000,
         config = function ()
             require 'fluoromachine'.setup {
-                glow = false,
-                theme = 'fluoromachine'
+                glow = false, -- basically just highlighting
+                brightness = 0.15, -- defines how intense the glow is
+
+                -- theme = 'fluoromachine', -- classic purple and yellow
+                -- theme = 'delta', -- strawberry red and green
+                theme = 'retrowave', -- variation of the og but with more colors
+
+                -- transparent = 'full', -- by default this is "false"
+                -- I'm not sure I get what this goes.
+
             }
             vim.cmd.colorscheme 'fluoromachine'
             -- vim.cmd.colorscheme 'solas'
