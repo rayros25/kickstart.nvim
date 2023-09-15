b51e6cdc024edb0eb26c7555c895418f472abb79
Raymond <rjrosales2025@gmail.com>
2023-09-15 13:04:21 -0400

    add markdown snippets and stuff

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+
+-- for coding stuff
+vim.bo.tabstop     = 8    -- size of a hard tabstop (ts).
+vim.bo.shiftwidth  = 8    -- size of an indentation (sw).
+vim.bo.expandtab   = true -- always uses spaces instead of tab characters (et).
+vim.bo.softtabstop = 8    -- number of spaces a <Tab> counts for. When 0, feature is off (sts)
+
+-- to look nice
+vim.cmd("SoftPencil")
+vim.cmd("colorscheme material-lighter")
+
