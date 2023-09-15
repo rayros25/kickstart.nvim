3610e527443e97999f445a4bb0535814dd3b141f
Raymond <rjrosales2025@gmail.com>
2023-09-15 12:58:48 -0400

    changed some comments and Zen

--- lua/custom/plugins/comments.lua
+++ lua/custom/plugins/comments.lua
@@ -10,9 +10,16 @@ return {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
         },
     },
+    -- {
+    --     'numToStr/Comment.nvim',
+    --     opts = {
+    --         -- add any options here
+    --     },
+    --     lazy = false,
+    -- }
 }
--- lua/custom/plugins/zen.lua
+++ lua/custom/plugins/zen.lua
@@ -7,5 +7,8 @@ return {
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
         }
+    },
+    {
+        "junegunn/goyo.vim",
     }
 }
